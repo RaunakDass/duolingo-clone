8360ff5f4a330889ce831541744894c74920ea25
Raunak Das <dasraunak5@gmail.com>
2024-03-25 14:21:59 +0530

    Added new queries and schema

--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,24 @@
 import { cache } from "react"
 import db from "./drizzle";
 
+export const getUserProgress = cache(aysnc () => {
+    const {userId} = await auth()
+
+    if(!userId){
+        return null
+    }
+
+    const data = await db.query.userProgress.findFirst({
+        where: eq(userProgress.userId, userId),
+        with: {
+            activeCourse: true,
+        }
+    })
+
+    return data
+})
+
+
 export const getCourses = cache(async () => {
     const data = await db.query.courses.findMany();
 
