3ee62bfb27e60c000abd6eb7d1957239788c9851
Raunak Das <dasraunak5@gmail.com>
2024-03-27 01:55:20 +0530

    Added new relations

--- db/schema.ts
+++ db/schema.ts
@@ -5,11 +5,44 @@ export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
   title: text("title").notNull(),
   imageSrc: text("image_src").notNull(),
-});
+})
+
 
 export const coursesRelations = relations(courses, ({many}) => ({
   userProgress: many(userProgress),
+  units: many(units),
 }))
+
+
+export const units = pgTable("units", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  courseId: integer("course_id").references(() => courses.id, {onDelete: "cascade"}).notNull(),
+  order: integer("order").notNull()
+})
+
+const unitRelations = relations(units, ({many,one}) =>({
+  course: one(courses, {
+    fields: [units.courseId],
+    references: [courses.id],
+  }),
+}))
+
+export const lessons = pgTable("lessons", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  unitId: integer("unit_id").references(() => units.id, {onDelete: "cascade"}).notNull(),
+  order: integer("order").notNull()
+})
+
+export const lessonsRelations = relations(lessons, ({one,many}) => ({
+  unit: one(units, {
+    fields: [lessons.unitId],
+    references: [units.id],
+  })
+}))
+
 
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
