df2200b47e3b34f17b46d2145ddf15e077778f39
Raunak Das <dasraunak5@gmail.com>
2024-04-18 10:44:51 +0530

    Added lessonId

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Toaster } from '@/components/ui/sonner';
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import { ExitModal } from '@/components/modals/exit-modal';
+import { HeartsModal } from "@/components/modals/hearts-modal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={font.className}>
         <Toaster />
         <ExitModal />
+        <HeartsModal />
         {children}
         </body>
       </html>
